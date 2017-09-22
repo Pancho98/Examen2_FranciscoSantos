9bae38b16c2eb3467da19b60e54d60438d4403d8
Francisco Santos <fjgsantos98@gmail.com>
2017-09-22 13:27:13 -0600

    Creacion de clase Usuario y Canciones

---
+++ Examen2_Francisco/src/examen2_francisco/Usuarios.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2_francisco;
+
+/**
+ *
+ * @author Francisco Santos
+ */
+public class Usuarios {
+     private String nombre, username, password;
+     private int edad;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(String nombre, String username, String password, int edad) {
+        this.nombre = nombre;
+        this.username = username;
+        this.password = password;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+     
+     
+     
+}
